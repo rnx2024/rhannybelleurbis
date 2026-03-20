2f57320c4b1756045ad80b9cd2ee552942712944
Rhanny Belle Urbis <raniurbis@gmail.com>
2026-03-20 17:42:34 +0800

    add-workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const repo = "/rhannybelleurbis";
+
 const nextConfig: NextConfig = {
   output: "export",
+  trailingSlash: true,
+  basePath: isProd ? repo : "",
 };
 
 export default nextConfig;
