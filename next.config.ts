50ff53367387fb3fd8bac8a76f5d409b9303fdc3
Rhanny Belle Urbis <raniurbis@gmail.com>
2026-03-23 21:21:17 +0800

    add-logo

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,9 @@ const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   basePath: isProd ? repo : "",
+  images: {
+    unoptimized: true,
+  },
   turbopack: {
     root: process.cwd(),
   },
