00b16b2fbb837356baa90410d3d37b00a1a7c9f0
Rhanny Belle Urbis <raniurbis@gmail.com>
2026-03-26 19:33:44 +0800

    add-work-experience-resume-download-2

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,16 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
+const isGithubPages = process.env.GITHUB_PAGES === "true";
 const repo = "/rhannybelleurbis";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true,
-  basePath: isProd ? repo : "",
+  ...(isGithubPages
+    ? {
+        output: "export",
+        trailingSlash: true,
+        basePath: repo,
+      }
+    : {}),
   images: {
     unoptimized: true,
   },
